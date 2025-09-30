255aa1508022ee4e6bdce4a885a7821718137857
sargis-2000 <sargis_davtyan_2014@mail.ru>
2025-09-30 19:48:38 +0400

    finished person task

--- 2-person.js
+++ 2-person.js
@@ -1,30 +1,32 @@
-function Person(name, age) {
-    this.name = name
-    this.age = age
-    this.stomach = []
-}
-
-Person.prototype.eat = function (food) {
-    if (this.stomach.length < 10) {
-        this.stomach.push(food)
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+        this.stomach = []
     }
-}
 
-Person.prototype.poop = function () {
-    this.stomach = []
-}
+    eat(food) {
+        if (this.stomach.length < 10) {
+            this.stomach.push(food)
+        }
+    }
 
-Person.prototype.toString = function () {
-    return `${this.name}, ${this.age}`
-}
+    poop() {
+        this.stomach = []
+    }
 
-Person.prototype.compareAge = function (otherPerson) {
-    if (this.age > otherPerson.age) {
-        return `${otherPerson.name} is younger than me.`
-    } else if (this.age < otherPerson.age) {
-        return `${otherPerson.name} is older than me.`
-    } else {
-        return `${otherPerson.name} is the same age as me`
+    toString() {
+        return `${this.name}, ${this.age}`
+    }
+
+    compareAge(otherPerson) {
+        if (this.age > otherPerson.age) {
+            return `${otherPerson.name} is younger than me.`
+        } else if (this.age < otherPerson.age) {
+            return `${otherPerson.name} is older than me`
+        } else {
+            return `${otherPerson.name} is the same age as me`
+        }
     }
 }
 
@@ -44,3 +46,33 @@ console.log(p1.stomach) // [ 'apple', 'sandwich' ]
 
 p1.poop()
 console.log(p1.stomach) // []
+
+// function Person(name, age) {
+//     this.name = name
+//     this.age = age
+//     this.stomach = []
+// }
+
+// Person.prototype.eat = function (food) {
+//     if (this.stomach.length < 10) {
+//         this.stomach.push(food)
+//     }
+// }
+
+// Person.prototype.poop = function () {
+//     this.stomach = []
+// }
+
+// Person.prototype.toString = function () {
+//     return `${this.name}, ${this.age}`
+// }
+
+// Person.prototype.compareAge = function (otherPerson) {
+//     if (this.age > otherPerson.age) {
+//         return `${otherPerson.name} is younger than me.`
+//     } else if (this.age < otherPerson.age) {
+//         return `${otherPerson.name} is older than me.`
+//     } else {
+//         return `${otherPerson.name} is the same age as me`
+//     }
+// }
